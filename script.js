7198c13bd39f688d478f7d92015f7686c0cfa33b
himanshu0072 <himanshu2003prajapati@gmail.com>
2024-03-29 00:47:20 +0530

    Array sorter is raedy to use

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-function selectData() {
-  var inputField = document.getElementById("arrays");
+function sortAscending() {
+  let inputArray = document
+    .getElementById("arrays")
+    .value.split(",")
+    .map((item) => Number(item.trim()));
+  let sortedArray = inputArray.sort((a, b) => a - b);
+  document.getElementById("sortedOutput").textContent =
+    "{" + sortedArray.toString() + "}";
+}
 
-  inputField.select();
-
-  let arrays = inputField.value;
-
-  console.log(arrays);
+function sortDescending() {
+  let inputArray = document
+    .getElementById("arrays")
+    .value.split(",")
+    .map((item) => Number(item.trim()));
+  let sortedArray = inputArray.sort((a, b) => b - a);
+  document.getElementById("sortedOutput").textContent =
+    "{" + sortedArray.toString() + "}";
 }
